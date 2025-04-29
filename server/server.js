3760ba31c92e23709f1903fd680416f5f5b6e774
Sahoo999 <sahoodeb04@gmail.comm>
2025-04-30 00:51:15 +0530

    server-modi

--- server/server.js
+++ server/server.js
@@ -22,5 +22,5 @@ app.use(cors())
  const PORT = process.env.PORT || 5000
 
  app.listen(PORT, ()=>{
-   console.log(`Server is running on port ${PORT}`)
+   console.log(`Server is running on port ${PORT}`);
  })
