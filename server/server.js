3500a0f05dc478c4c51a20341280707a2eac0f15
Debangsu Sahoo <sahoodeb04@gmail.comm>
2026-08-22 13:03:07 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -1,57 +1,37 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
-
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRouter.js'
 import userRouter from './routes/userRoutes.js'
 
-// Initialize Express
-const app = express()
 
-// Connect to database and Cloudinary
+//initialize exp
+const app= express()
+
+// connect to db
 await connectDB()
 await connectCloudinary()
 
-// CORS
-const allowedOrigins = [
-    'https://lms-frontend-steel-six.vercel.app',
-    'http://localhost:5173'
-];
-
-app.use(
-    cors({
-        origin: allowedOrigins,
-        credentials: true,
-    })
-);
-
-// Clerk middleware
+//midwares
+app.use(cors())
 app.use(clerkMiddleware())
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('API Working')
-})
+//route
+ app.get('/', (req, res)=> res.send("API Working"))
+ app.post('/clerk', express.json(), clerkWebhooks)
+ app.use('/api/educator', express.json(), educatorRouter)
+ app.use('/api/course', express.json(), courseRouter)
+ app.use('/api/user', express.json(),userRouter)
+ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
-app.post('/clerk', express.json(), clerkWebhooks)
+ //port
+ const PORT = process.env.PORT || 5000
 
-app.use('/api/educator', express.json(), educatorRouter)
-
-app.use('/api/course', express.json(), courseRouter)
-
-app.use('/api/user', express.json(), userRouter)
-
-app.post(
-    '/stripe',
-    express.raw({ type: 'application/json' }),
-    stripeWebhooks
-)
-
-// Export for Vercel
-export default app
+ app.listen(PORT, ()=>{
+   console.log(`Server is running on port ${PORT}`);
+ })
