099814a030c4f5a20c3c6b3ec13fde20c53ea958
Sahoo999 <sahoodeb04@gmail.comm>
2026-08-22 10:24:42 +0530

    db fixed

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
 
-// connect to the db
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-const connectDB = async ()=>{
-    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
-}
-export default connectDB
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => {
+        console.log("MongoDB Connected");
+    });
+
+    mongoose.connection.on("error", (error) => {
+        console.error("MongoDB Connection Error:", error);
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+};
+
+export default connectDB;
