fe209737cf3282dc2a9cde8d666d63e94555d52f
Sahoo999 <sahoodeb04@gmail.comm>
2026-08-22 11:58:22 +0530

    page bug fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+
 import App from './App.jsx';
+
 import { AppContextProvider } from './context/AppContext.jsx';
+
 import { BrowserRouter } from 'react-router-dom';
-import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+    throw new Error('Missing Publishable Key');
 }
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-      <SignedIn>
-        <AppContextProvider>
-          <App />
-        </AppContextProvider>
-      </SignedIn>
-      <SignedOut>
-      <RedirectToSignIn />
-      </SignedOut>
-    </ClerkProvider>
-  </BrowserRouter>
+    <React.StrictMode>
+        <ClerkProvider
+            publishableKey={PUBLISHABLE_KEY}
+            afterSignOutUrl="/"
+        >
+            <BrowserRouter>
+                <AppContextProvider>
+                    <App />
+                </AppContextProvider>
+            </BrowserRouter>
+        </ClerkProvider>
+    </React.StrictMode>
 );
